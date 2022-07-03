e049597f5c1c48ca137bab1381a8380fb8ddf31c
Danila Ryabov <danila02478@gmail.com>
2022-07-03 17:55:17 +0300

    fixed ShowStats

--- src/main/java/ru/javarush/island/ryabov/constants/Constants.java
+++ src/main/java/ru/javarush/island/ryabov/constants/Constants.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 
 public class Constants {
-    public static final int ROWS = 2;
-    public static final int COLS = 2;
+    public static final int ROWS = 20;
+    public static final int COLS = 100;
 
     public static final List<Class<?>> TYPES = ClassFinder.find("ru.javarush.island.ryabov.entity.organisms.organism");
     public static final Organism[] ORGANISMS = EntityScanner.createPrototypes(TYPES);
